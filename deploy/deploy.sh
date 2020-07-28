1569cab55809f63beee2c0cff87b3be4f7e6e8bf
ruaya <1628331231@qq.com>
2020-07-28 10:54:08 +0800

    update

---
+++ deploy/deploy.sh
@@ -0,0 +1,3 @@
+git pull
+yarn build
+git push
---
+++ deploy/push.sh
@@ -0,0 +1,5 @@
+git add ../
+git commit -m update
+git remote remove origin
+git remote add origin git@github.com:ruaya/blog-server.git
+git push -u origin master
